0445a631b815bcea50ae5f4885efdb9d446177f6
pedroaurelli <pedroaureliano.contato@gmail.com>
2024-06-06 21:18:56 -0300

    Add migrations

---
+++ backend/Database/Migrations/20240607001619_RenameModelToAudio.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameModelToAudio : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "files_storage");
+
+            migrationBuilder.CreateTable(
+                name: "audios",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    bucket = table.Column<string>(type: "text", nullable: false),
+                    key = table.Column<string>(type: "text", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_audios", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "audio_texts",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    audio_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    text = table.Column<string>(type: "text", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_audio_texts", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_audio_texts_audios_audio_id",
+                        column: x => x.audio_id,
+                        principalTable: "audios",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_audio_texts_audio_id",
+                table: "audio_texts",
+                column: "audio_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "audio_texts");
+
+            migrationBuilder.DropTable(
+                name: "audios");
+
+            migrationBuilder.CreateTable(
+                name: "files_storage",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    bucket = table.Column<string>(type: "text", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    key = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_files_storage", x => x.id);
+                });
+        }
+    }
+}
