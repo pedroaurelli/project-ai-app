67e3e588f2f291934ad633e575aa32604fd18c18
pedroaurelli <pedroaureliano.contato@gmail.com>
2024-06-07 11:49:33 -0300

    Create AudioTranscriptions setup

---
+++ backend/Services/AudioTranscriptions/TranscribeAudio/TranscribeAudioService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using OpenAI_API;
+using Services.Audios.ConvertAudioToStream;
+
+namespace Services.AudioTranscriptions.TranscribeAudio;
+
+public class TranscribeAudioService : Service
+{
+    private readonly OpenAIAPI _openAIAPI;
+    private readonly ConvertAudioToStreamService _convertAudioToStreamService;
+
+    public TranscribeAudioService(
+        OpenAIAPI openAIAPI,
+        ConvertAudioToStreamService convertAudioToStreamService)
+    {
+        _openAIAPI = openAIAPI;
+        _convertAudioToStreamService = convertAudioToStreamService;
+    }
+
+    public async Task<string> TranscribeAudioAsync(
+        IFormFile file)
+    {
+        var stream = await _convertAudioToStreamService.ConvertAudioToStreamAsync(file);
+
+        var transcription = await _openAIAPI.Transcriptions.GetTextAsync(
+            stream,
+            file.FileName);
+
+        return transcription;
+    }
+}
---
+++ backend/project-ai/Controllers/AudioTranscriptionsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.AudioTranscriptions.TranscribeAudio;
+
+namespace project_ai.Controllers;
+
+[Route("audio-transcriptions")]
+public class AudioTranscriptionsController : Controller
+{
+    [HttpPost("transcribe")]
+    public async Task<IActionResult> TranscribeAudioAsync(
+        [FromServices] TranscribeAudioService transcribeAudioService,
+        IFormFile file)
+    {
+        var transcription = await transcribeAudioService.TranscribeAudioAsync(file);
+
+        return Ok(transcription);
+    }
+}
