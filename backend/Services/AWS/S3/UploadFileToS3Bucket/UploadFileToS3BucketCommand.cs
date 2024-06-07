8d6a6c05ad99e7f3b6e256b5e9878b6f856c6457
pedroaurelli <pedroaureliano.contato@gmail.com>
2024-06-07 11:47:06 -0300

    Create UploadFileToS3Bucket service

---
+++ backend/Services/AWS/S3/UploadFileToS3Bucket/UploadFileToS3BucketCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace Services.AWS.S3.UploadFileToS3Bucket;
+
+public class UploadFileToS3BucketCommand
+{
+    public required string Bucket { get; set; }
+
+    public required string ObjectKey { get; set; } = string.Empty;
+
+    public required string ContentType { get; set; } = string.Empty;
+
+    public string FilePath { get; set; } = string.Empty;
+
+    public required Stream Stream { get; set; }
+}
---
+++ backend/Services/AWS/S3/UploadFileToS3Bucket/UploadFileToS3BucketService.cs
@@ -0,0 +1,48 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace Services.AWS.S3.UploadFileToS3Bucket;
+
+public class UploadFileToS3BucketService : Service
+{
+    private IAmazonS3 awsS3Client;
+
+    public UploadFileToS3BucketService(IAmazonS3 awsS3Client)
+    {
+        this.awsS3Client = awsS3Client;
+    }
+
+    public async Task<Guid> UploadToS3BucketAsync(
+        UploadFileToS3BucketCommand command,
+        CancellationToken cancellationToken = default)
+    {
+        var objectKey = command.ObjectKey;
+        var bucketName = command.Bucket;
+        var stream = command.Stream;
+        var contentType = command.ContentType;
+
+        var bucketToKebab = StringUtils.ToKebabCase(bucketName.ToString());
+        var request = new PutObjectRequest
+        {
+            BucketName = bucketToKebab,
+            Key = objectKey,
+            CannedACL = S3CannedACL.Private,
+            InputStream = stream,
+            ContentType = contentType
+        };
+
+        await awsS3Client.PutObjectAsync(request, cancellationToken);
+
+        var newAudio = new Audio
+        {
+            Key = objectKey,
+            Bucket = bucketName,
+            ContentType = contentType
+        };
+
+        DbContext.Audios.Add(newAudio);
+        await DbContext.SaveChangesAsync(cancellationToken);
+
+        return newAudio.Id;
+    }
+}
