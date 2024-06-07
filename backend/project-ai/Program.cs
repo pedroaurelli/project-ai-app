aec2ef8262b279e57b29b18dae96fdb00d367d0e
pedroaurelli <pedroaureliano.contato@gmail.com>
2024-06-07 11:46:09 -0300

    Add singleton services and aws configuration

--- backend/project-ai/Program.cs
+++ backend/project-ai/Program.cs
@@ -1,5 +1,10 @@
+using Amazon;
+using Amazon.Runtime;
+using Amazon.S3;
 using Database;
+using Services;
 using Microsoft.EntityFrameworkCore;
+using OpenAI_API;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,12 +14,35 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddServices();
 
 var configuration = builder.Configuration;
 
 builder.Services.AddDbContext<DatabaseContext>(options =>
 {
     options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Host.ConfigureServices((host, services) =>
+{
+    var awsSettings = host.Configuration.GetSection("AWS");
+    var credentials = new BasicAWSCredentials(
+        awsSettings.GetValue<string>("AccessKeyId"),
+        awsSettings.GetValue<string>("SecretAccessKey"));
+    var region = RegionEndpoint.GetBySystemName(
+        awsSettings.GetValue<string>("Region"));
+
+    var s3Config = new AmazonS3Config
+    {
+        ServiceURL = awsSettings.GetValue<string>("Endpoint"),
+        ForcePathStyle = true
+    };
+
+    var s3Client = new AmazonS3Client(credentials, s3Config);
+
+    services.AddSingleton<IAmazonS3>(s3Client);
+    services.AddSingleton(sg => new OpenAIAPI(
+        configuration.GetValue<string>("OpenAI:API_KEY")));
 });
 
 var app = builder.Build();
