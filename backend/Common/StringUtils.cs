3a37a9f0d3fa07501d6c1f5805554b292485cc35
pedroaurelli <pedroaureliano.contato@gmail.com>
2024-06-07 11:46:36 -0300

    Add StringUtils static class

---
+++ backend/Common/StringUtils.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Common;
+
+public static class StringUtils
+{
+    public static string ToKebabCase(string str)
+    {
+        return Regex.Replace(str, "(?<!^)([A-Z])", "-$1").ToLower();
+    }
+}
