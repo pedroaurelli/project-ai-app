13758be072910311afba8e145b9401981338f3a0
pedroaurelli <pedroaureliano.contato@gmail.com>
2024-06-07 11:47:21 -0300

    Create ConvertAudioToStream service

---
+++ backend/Services/Audios/ConvertAudioToStream/ConvertAudioToStreamResult.cs
@@ -0,0 +1,12 @@
+﻿namespace Services.Audios.ConvertAudioToStream;
+
+public class ConvertAudioToStreamResult
+{
+    public Guid Id { get; set; }
+
+    public string FileName { get; set; } = string.Empty;
+
+    public string ContentType { get; set; } = string.Empty;
+
+    public long StreamLength { get; set; }
+}
---
+++ backend/Services/Audios/ConvertAudioToStream/ConvertAudioToStreamService.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Services.AWS.S3.UploadFileToS3Bucket;
+
+namespace Services.Audios.ConvertAudioToStream;
+
+public class ConvertAudioToStreamService : Service
+{
+    private readonly UploadFileToS3BucketService _uploadFileToS3BucketService;
+
+    public ConvertAudioToStreamService(
+        UploadFileToS3BucketService uploadFileToS3BucketService)
+    {
+        this._uploadFileToS3BucketService = uploadFileToS3BucketService;
+    }
+
+    public async Task<Stream> ConvertAudioToStreamAsync(
+        IFormFile file,
+        CancellationToken cancellationToken = default)
+    {
+        if (file == null || file.Length == 0)
+        {
+            throw new Exception("File is empty");
+        }
+
+        var stream = new MemoryStream();
+        await file.CopyToAsync(stream, cancellationToken);
+        stream.Position = 0;
+
+        var clonedStream = new MemoryStream();
+        await stream.CopyToAsync(clonedStream, cancellationToken);
+        clonedStream.Position = 0;
+
+        using (var reader = new StreamReader(stream, leaveOpen: true))
+        {
+            var content = await reader.ReadToEndAsync(cancellationToken);
+        }
+
+        Console.WriteLine($"stream lenght: {stream.Length}");
+
+        var contentToAws = new UploadFileToS3BucketCommand
+        {
+            Bucket = "audios",
+            ObjectKey = file.FileName,
+            Stream = stream,
+            ContentType = file.ContentType
+        };
+
+        await _uploadFileToS3BucketService.UploadToS3BucketAsync(contentToAws, cancellationToken);
+
+        return clonedStream;
+    }
+}
+
