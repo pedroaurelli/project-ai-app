4b09cecca3ded0f728bc5a0394483a48c9b6864e
pedroaurelli <pedroaureliano.contato@gmail.com>
2024-06-10 10:19:37 -0300

    Update TranscribeAudioService

--- backend/Services/AudioTranscriptions/TranscribeAudio/TranscribeAudioService.cs
+++ backend/Services/AudioTranscriptions/TranscribeAudio/TranscribeAudioService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json.Linq;
 using OpenAI_API;
 using Services.Audios.ConvertAudioToStream;
 
@@ -20,12 +21,36 @@ public class TranscribeAudioService : Service
     public async Task<string> TranscribeAudioAsync(
         IFormFile file)
     {
-        var stream = await _convertAudioToStreamService.ConvertAudioToStreamAsync(file);
+        var audioToStream = await _convertAudioToStreamService.ConvertAudioToStreamAsync(file);
 
         var transcription = await _openAIAPI.Transcriptions.GetTextAsync(
-            stream,
+            audioToStream.Stream,
             file.FileName);
 
-        return transcription;
+        if (transcription == null)
+        {
+            throw new Exception("Failed to transcribe audio.");
+        }
+
+        var audioTranscription = new AudioTranscription
+        {
+            AudioId = audioToStream.AudioId,
+            Text = transcription
+        };
+        DbContext.Add(audioTranscription);
+
+        var modelDetails = await _openAIAPI.Models.RetrieveModelDetailsAsync("ft:davinci-002:personal:project-ai-model:9XXzUFOa");
+        _openAIAPI.Completions.DefaultCompletionRequestArgs.Model = modelDetails.ModelID;
+        _openAIAPI.Completions.DefaultCompletionRequestArgs.MaxTokens = 100;
+        _openAIAPI.Completions.DefaultCompletionRequestArgs.Temperature = 0;
+
+        var completion = await _openAIAPI.Completions.CreateCompletionAsync(transcription);
+
+        var response = completion.Completions[0].ToString();
+        var json = JObject.Parse(response);
+
+        await DbContext.SaveChangesAsync();
+
+        return json.ToString(Newtonsoft.Json.Formatting.None);
     }
 }
